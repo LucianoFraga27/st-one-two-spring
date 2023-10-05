e3e170498182bec16bc2abb2d21efd7ad10dfc16
LucianoFraga27 <luciano.lucas@bllc.com.br>
2023-10-04 21:15:13 -0300

    first commit

---
+++ src/test/java/com/stoica/onetwo/OnetwoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.stoica.onetwo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OnetwoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
